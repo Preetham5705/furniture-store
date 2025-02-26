7d34cf6fea1ac68a1b1b1875a28595002ac8a088
Preetham Reddy <gurupreethamreddypatnam@gmail.com>
2025-02-27 03:00:50 +0530

    Updated image paths

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
+import ImageSlider from "./components/ImageSlider";
 
 function App() {
     return (
         <>
             <Navbar />
             <HeroSection />
+            <ImageSlider />
             <AboutSection />
         </>
     );
